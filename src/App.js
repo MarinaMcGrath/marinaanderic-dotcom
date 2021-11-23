fe93e40110f5223cf90af4aa56b99b79e693c94a
MarinaMcGrath <marinamcgrath92@gmail.com>
2021-11-22 19:45:51 -0800

    base app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Hero">
+          <p className="name">
+            Marina and Eric
+          </p>
+          <p className="icons">
+            October 1, 2022 | Georgetown, TN
+          </p>
+        </div>
       </header>
     </div>
   );
